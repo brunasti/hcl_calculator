32578ae9f8e4d202c21f649cce871c5be01038cc
Paolo Brunasti <paolo@brunasti.it>
2021-01-31 17:20:44 +0100

    SimpleCalculator tests

--- src/main/java/it/brunasti/hcl/calculator/logic/SimpleCalculator.java
+++ src/main/java/it/brunasti/hcl/calculator/logic/SimpleCalculator.java
@@ -6,19 +6,19 @@ import lombok.Data;
 public class SimpleCalculator {
 
     public double add(int a, int b) {
-        return 0;
+        return a + b;
     }
 
     public double subtract(int a, int b) {
-        return 0;
+        return a - b;
     }
 
     public double multiply(int a, int b) {
-        return 0;
+        return a * b;
     }
 
     public double divide(int a, int b) {
-        return 0;
+        return a / b;
     }
 
 
--- src/test/java/it/brunasti/hcl/calculator/logic/SimpleCalculatorTests.java
+++ src/test/java/it/brunasti/hcl/calculator/logic/SimpleCalculatorTests.java
@@ -7,10 +7,56 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SimpleCalculatorTests {
 
     @Test
-    public void mapper_dumb() {
+    public void add() {
         SimpleCalculator simpleCalculator = new SimpleCalculator();
 
         assertEquals(0, simpleCalculator.add(0,0));
+        assertEquals(1, simpleCalculator.add(0,1));
+        assertEquals(1, simpleCalculator.add(1,0));
+        assertEquals(2, simpleCalculator.add(1,1));
+        assertEquals(0, simpleCalculator.add(1,-1));
+    }
+
+    @Test
+    public void subtract() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        assertEquals(0, simpleCalculator.subtract(0,0));
+        assertEquals(-1, simpleCalculator.subtract(0,1));
+        assertEquals(0, simpleCalculator.subtract(1,1));
+    }
+
+    @Test
+    public void multiply() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        assertEquals(0, simpleCalculator.multiply(0,0));
+        assertEquals(0, simpleCalculator.multiply(0,1));
+        assertEquals(0, simpleCalculator.multiply(1,0));
+        assertEquals(1, simpleCalculator.multiply(1,1));
+        assertEquals(1, simpleCalculator.multiply(-1,-1));
+        assertEquals(-1, simpleCalculator.multiply(1,-1));
+        assertEquals(-1, simpleCalculator.multiply(-1,1));
+    }
+
+    @Test
+    public void divide() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        assertEquals(0, simpleCalculator.divide(0,1));
+        assertEquals(1, simpleCalculator.divide(1,1));
+        assertEquals(1, simpleCalculator.divide(-1,-1));
+        assertEquals(-1, simpleCalculator.divide(1,-1));
+        assertEquals(-1, simpleCalculator.divide(-1,1));
+
+        assertThrows(ArithmeticException.class, () -> {
+            assertEquals(0, simpleCalculator.divide(0,0));
+        });
+
+        assertThrows(ArithmeticException.class, () -> {
+            assertEquals(0, simpleCalculator.divide(1,0));
+        });
+
     }
 
 
